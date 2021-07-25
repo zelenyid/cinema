f25c384ee08b73a2c13aa8a9a12f2984eb5f2db2
Zelenyi Dmytro <zelenyid446@gmail.com>
2021-07-25 18:11:47 +0300

    Add annotation to models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  imdb_id    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Movie < ApplicationRecord
   has_many :showtimes, dependent: :destroy
 end
--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: showtimes
+#
+#  id         :bigint           not null, primary key
+#  movie_id   :bigint
+#  time       :datetime
+#  price      :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Showtime < ApplicationRecord
   belongs_to :movie
 end
