9a6efbabbc1b74ebd54a483d9bfbd095e432f4d0
Zelenyi Dmytro <zelenyid446@gmail.com>
2021-07-25 19:23:30 +0300

    Updated annotate

--- spec/models/showtime_spec.rb
+++ spec/models/showtime_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: showtimes
+#
+#  id         :bigint           not null, primary key
+#  movie_id   :bigint
+#  time       :datetime
+#  price      :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Showtime, type: :model do
